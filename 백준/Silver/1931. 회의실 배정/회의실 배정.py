b095c90c3b0ac52945eb7cf5360096f7091d2f10
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-06-06 19:10:33 +0900

    [Silver I] Title: 회의실 배정, Time: 384 ms, Memory: 121528 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -4,15 +4,15 @@ input = sys.stdin.readline
 n = int(input())
 mt = []
 for _ in range(n):
-    s, e = map(int, input().split())
-    mt.append((s,e))
+    st, ed = map(int, input().split())
+    mt.append((st, ed))
 mt.sort(key=lambda x: (x[1], x[0]))
 
 last_end = 0
-mx = 0
+cnt = 0
 
 for i, j in mt:
     if last_end <= i:
-        mx += 1
+        cnt += 1
         last_end = j
-print(mx)
+print(cnt)
