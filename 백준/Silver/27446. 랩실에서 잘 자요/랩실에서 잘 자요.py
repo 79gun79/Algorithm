ed8a3444552c84ebc70284ad800ae6ded111959d
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-07-21 18:44:06 +0900

    [Silver III] Title: 랩실에서 잘 자요, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.py
+++ 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.py
@@ -2,21 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-num = list(map(int, input().split()))
+bd = list(map(int, input().split()))
+pg = [i+1 for i in range(n)]
+pr = [j for j in pg if j not in bd]
 
-prt = []
-for i in range(1, n+1):
-    if i not in num:
-        prt.append(i)
-
-idx = 0
 ink = 0
-for j in range(len(prt)):
-    if j == 0:
+for k in range(len(pr)):
+    if k == 0:
         ink += 7
-        idx = prt[0]
     else:
-        ink += min(7, 2*(prt[j] - idx))
-        idx = prt[j]
+        ink += min(7, 2*(pr[k] - pr[k-1]))
 
 print(ink)
+
