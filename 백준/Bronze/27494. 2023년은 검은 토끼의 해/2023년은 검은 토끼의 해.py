ee3105e98c9ebb1f4d76a91de73c779e9e37e327
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-08-20 14:32:03 +0900

    [Bronze I] Title: 2023년은 검은 토끼의 해, Time: 2700 ms, Memory: 110920 KB -BaekjoonHub

--- 백준/Bronze/27494. 2023년은 검은 토끼의 해/2023년은 검은 토끼의 해.py
+++ 백준/Bronze/27494. 2023년은 검은 토끼의 해/2023년은 검은 토끼의 해.py
@@ -2,14 +2,13 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
+res = 0
 if n < 2023:
-    print(0)
+    res = 0
 else:
-    cnt = 0
     for i in range(2023, n+1):
         i = str(i)
-        if not {'0', '2', '3'}.issubset(set(i)):
+        if not {'0','2','3'}.issubset(set(i)):
             continue
         tmp = []
         for j in i:
@@ -18,7 +17,6 @@ else:
             elif j == '0' and len(tmp) == 1:
                 tmp.append(j)
             elif j == '3' and len(tmp) == 3:
-                cnt += 1
+                res += 1
                 break
-    print(cnt)
-
+print(res)
