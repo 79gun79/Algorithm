748c149acbecff89f300c9e2005afc268cbdb195
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-09-02 13:38:16 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 276 ms, Memory: 121452 KB -BaekjoonHub

--- 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
@@ -2,20 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-chair = [list(map(int,input().split())) for _ in range(n)]
+ch = [list(map(int, input().split())) for _ in range(n)]
 
 def HCPC(x, y, n):
     if n < 2:
-        return chair[x][y]
+        return ch[x][y]
 
     else:
-        num = []
-        num.append(HCPC(x, y, n//2))
-        num.append(HCPC(x, y + n//2, n//2))
-        num.append(HCPC(x + n//2, y, n//2))
-        num.append(HCPC(x + n//2, y + n//2, n//2))
+        a = HCPC(x, y, n//2)
+        b = HCPC(x, y + n//2, n//2)
+        c = HCPC(x + n//2, y, n//2)
+        d = HCPC(x + n//2, y + n//2, n//2)
+        num = [a, b, c, d]
         num.sort()
 
         return num[1]
 
-print(HCPC(0, 0, n))
+print(HCPC(0,0, n))
