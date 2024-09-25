d9998d7226eb8efdd348257c371f22ca1a538382
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-09-25 21:19:26 +0900

    [Bronze I] Title: 돌림판 문자열, Time: 76 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/25705. 돌림판 문자열/돌림판 문자열.py
+++ 백준/Bronze/25705. 돌림판 문자열/돌림판 문자열.py
@@ -2,26 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-t = input().rstrip()
+ns = input().rstrip()
 m = int(input())
-s = input().rstrip()
+ms = input().rstrip()
 
-cnt, j = 0, 0
-br_cnt = 0
-sr = ''
+i, j, k = 0, 0, 0
+res = ''
 while True:
-    cnt += 1
-    br_cnt += 1
-    i = (cnt-1) % n
-    if t[i] == s[j]:
-        br_cnt = 0
-        sr += t[i]
-        j += 1
 
-    if br_cnt > n:
+    i += 1
+    j += 1
+    a = (i-1) % n
+
+    if ns[a] == ms[k]:
+        j = 0
+        k += 1
+        res += ns[a]
+
+    if j > n:
         print(-1)
         break
 
-    if sr == s:
-        print(cnt)
+    if ms == res:
+        print(i)
         break
