88fdbdaa4ecff75403b6953872d8bc9bd798555d
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-10-10 15:19:56 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 88 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.py
@@ -1,26 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-def factorial(x, k):
-    res = x
-    for _ in range(k-1):
-        x -= 1
-        if x < 1:
-            return 0
+def fact(x, k):
+    res = 1
+    for _ in range(k):
         res *= x
+        x -= 1
+        if x < 0:
+            return 0
     return res
 
 m = int(input())
-st = list(map(int,input().split()))
+st = list(map(int, input().split()))
 k = int(input())
-
-if k == 1:
-    print(1.0)
-    quit()
 
 boonja = 0
 for i in st:
-    boonja += factorial(i, k)
-boonmo = factorial(sum(st), k)
+    boonja += fact(i, k)
+boonmo = fact(sum(st), k)
 
 print(boonja/boonmo)
