611c1157588be1c9fe7577c5f8a4b608971e50ab
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-10-11 13:15:51 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 224 ms, Memory: 127848 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -2,28 +2,27 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n = int(input())
-tr = [[0] for _ in range(100001)]
-visited = [0] * (100001)
-
-def bfs():
+def bfs(a):
     q = deque()
-    visited[1] = 1
-    q.append(1)
+    q.append(a)
+    visit[a] = 1
 
     while q:
         x = q.popleft()
         for i in tr[x]:
-            if visited[i] == 0:
-                visited[i] = x
+            if visit[i] == 0:
+                visit[i] = x
                 q.append(i)
 
 
+
+n = int(input())
+tr = [[] for _ in range(n+1)]
+visit = [0] * (n+1)
 for _ in range(n-1):
     a, b = map(int, input().split())
     tr[a].append(b)
     tr[b].append(a)
-
-bfs()
+bfs(1)
 for j in range(2, n+1):
-    print(visited[j])
+    print(visit[j])
