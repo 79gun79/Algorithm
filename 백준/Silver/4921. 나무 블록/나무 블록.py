02af070e976ad906c632ed7624e7c3e580db3247
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-11-24 23:11:51 +0900

    [Silver IV] Title: 나무 블록, Time: 144 ms, Memory: 110416 KB -BaekjoonHub

--- 백준/Silver/4921. 나무 블록/나무 블록.py
+++ 백준/Silver/4921. 나무 블록/나무 블록.py
@@ -2,73 +2,35 @@ import sys
 input = sys.stdin.readline
 
 idx = 0
+namu = {'3': ['4', '5'], '4': ['2', '3'], '5': ['8'], '6': ['2', '3'], '7': ['8'], '8': ['6', '7']}
 while True:
-    c = input().rstrip()
     idx += 1
-
-    if c == '0':
+    a = input().rstrip()
+    if a == '0':
         break
 
-    if len(c) < 1:
+    if a[0] != '1':
         print(f'{idx}. NOT')
         continue
-
-    if len(c) > 10000:
+    if a[-1] != '2':
         print(f'{idx}. NOT')
         continue
-
-    if c[0] != '1':
-        print(f'{idx}. NOT')
-        continue
-
-    if c[-1] != '2':
-        print(f'{idx}. NOT')
-        continue
-
-    if c[1] not in ['4', '5']:
+    if a[1] not in ['4', '5']:
         print(f'{idx}. NOT')
         continue
 
 
     flag = True
-    for i in range(1, len(c)-1):
-        if c[i] in ['1', '2']:
+    for i in range(1, len(a)-1):
+        if a[i] not in namu.keys():
             flag = False
             break
-
-        if c[i] == '3':
-            if c[i+1] not in ['4', '5']:
-                flag = False
-                continue
-
-        if c[i] == '4':
-            if c[i+1] not in ['2', '3']:
+        else:
+            if a[i+1] not in namu[a[i]]:
                 flag = False
                 break
-
-        if c[i] == '5':
-            if c[i+1] != '8':
-                flag = False
-                break
-
-        if c[i] == '6':
-            if c[i+1] not in ['2', '3']:
-                flag = False
-                break
-
-        if c[i] == '7':
-            if c[i+1] != '8':
-                flag = False
-                break
-
-        if c[i] == '8':
-            if c[i+1] not in ['6', '7']:
-                flag = False
-                break
-
 
     if flag:
         print(f'{idx}. VALID')
     else:
         print(f'{idx}. NOT')
-
