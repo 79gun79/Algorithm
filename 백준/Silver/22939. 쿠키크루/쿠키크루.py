632494efde37b8e30f7bb2f0d584f4687360757b
j_gun2 <127276008+79gun79@users.noreply.github.com>
2024-12-04 16:55:38 +0900

    [Silver I] Title: 쿠키크루, Time: 96 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/22939. 쿠키크루/쿠키크루.py
+++ 백준/Silver/22939. 쿠키크루/쿠키크루.py
@@ -2,70 +2,38 @@ import sys
 input = sys.stdin.readline
 from itertools import permutations
 
+job = ['Assassin', 'Healer', 'Mage', 'Tanker']
 n = int(input())
 mp = [input().rstrip() for _ in range(n)]
 
-Assassin = []
-Healer = []
-Mage = []
-Tanker = []
+job_dis = {'J': [], 'C': [], 'B': [], 'W': []}
 Home = []
 Goal = []
-
 for i in range(n):
     for j in range(n):
-        if mp[i][j] == 'J':
-            Assassin.append([i, j])
-        elif mp[i][j] == 'C':
-            Healer.append([i, j])
-        elif mp[i][j] == 'B':
-            Mage.append([i, j])
-        elif mp[i][j] == 'W':
-            Tanker.append([i, j])
+        if mp[i][j] in job_dis.keys():
+            job_dis[mp[i][j]].append([i, j])
         elif mp[i][j] == 'H':
             Home = [i, j]
         elif mp[i][j] == '#':
             Goal = [i, j]
 
-Assassin_dis = sys.maxsize
-for k in list(permutations(Assassin, 3)):
-    tmp = 0
-    tmp += abs(Home[0] - k[0][0]) + abs(Home[1] - k[0][1])
-    tmp += abs(k[0][0] - k[1][0]) + abs(k[0][1] - k[1][1])
-    tmp += abs(k[1][0] - k[2][0]) + abs(k[1][1] - k[2][1])
-    tmp += abs(k[2][0] - Goal[0]) + abs(k[2][1] - Goal[1])
-    Assassin_dis = min(Assassin_dis, tmp)
-Healer_dis = sys.maxsize
-for k in list(permutations(Healer, 3)):
-    tmp = 0
-    tmp += abs(Home[0] - k[0][0]) + abs(Home[1] - k[0][1])
-    tmp += abs(k[0][0] - k[1][0]) + abs(k[0][1] - k[1][1])
-    tmp += abs(k[1][0] - k[2][0]) + abs(k[1][1] - k[2][1])
-    tmp += abs(k[2][0] - Goal[0]) + abs(k[2][1] - Goal[1])
-    Healer_dis = min(Healer_dis, tmp)
-Mage_dis = sys.maxsize
-for k in list(permutations(Mage, 3)):
-    tmp = 0
-    tmp += abs(Home[0] - k[0][0]) + abs(Home[1] - k[0][1])
-    tmp += abs(k[0][0] - k[1][0]) + abs(k[0][1] - k[1][1])
-    tmp += abs(k[1][0] - k[2][0]) + abs(k[1][1] - k[2][1])
-    tmp += abs(k[2][0] - Goal[0]) + abs(k[2][1] - Goal[1])
-    Mage_dis = min(Mage_dis, tmp)
-Tanker_dis = sys.maxsize
-for k in list(permutations(Tanker, 3)):
-    tmp = 0
-    tmp += abs(Home[0] - k[0][0]) + abs(Home[1] - k[0][1])
-    tmp += abs(k[0][0] - k[1][0]) + abs(k[0][1] - k[1][1])
-    tmp += abs(k[1][0] - k[2][0]) + abs(k[1][1] - k[2][1])
-    tmp += abs(k[2][0] - Goal[0]) + abs(k[2][1] - Goal[1])
-    Tanker_dis = min(Tanker_dis, tmp)
+def distance(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-Job = [Assassin_dis, Healer_dis, Mage_dis, Tanker_dis]
-Job_name = ['Assassin', 'Healer', 'Mage', 'Tanker']
-mn_dis = sys.maxsize
-idx = 0
-for l in range(4):
-    if mn_dis > Job[l]:
-        mn_dis = Job[l]
-        idx = l
-print(Job_name[idx])
+def search_dis(lst):
+    dis = sys.maxsize
+    for k in list(permutations(lst, 3)):
+        tmp = distance(Home, k[0]) + distance(k[0], k[1]) + distance(k[1], k[2]) + distance(k[2], Goal)
+        dis = min(dis, tmp)
+    return dis
+
+mn = sys.maxsize
+res = 0
+for i in range(4):
+    lst = list(job_dis.values())[i]
+    if mn > search_dis(lst):
+        mn = search_dis(lst)
+        res = i
+
+print(job[res])
