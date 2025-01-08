1460e3d1451758924c41327b26c319dddd495ffa
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-01-08 10:51:18 +0900

    [Silver II] Title: 회의실 배정 2, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/19621. 회의실 배정 2/회의실 배정 2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+met = [list(map(int, input().split())) for _ in range(n)]
+dp = [0] * n
+met.sort()
+
+dp[0] = met[0][2]
+
+for i in range(1, n):
+    dp[i] = max(dp[i-1], dp[i-2] + met[i][2])
+
+print(dp[n-1])
