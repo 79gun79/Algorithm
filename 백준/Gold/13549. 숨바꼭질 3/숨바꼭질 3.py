e567700afd7cf9f91d7417843e732b8f79e75ae0
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-01-08 16:53:21 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 112896 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(x):
+    q = deque()
+    q.append(x)
+    while q:
+        a = q.popleft()
+        da = [a - 1, a + 1, 2 * a]
+        if a == k:
+            return visited[a]
+        for i in da:
+            if i < 0 or i >= 100001:
+                continue
+            if visited[i] == 0:
+                if i == 2 * a and i != 0:
+                    visited[i] = visited[a]
+                    q.appendleft(i)
+                else:
+                    visited[i] = visited[a] + 1
+                    q.append(i)
+
+n, k = map(int, input().split())
+visited = [0] * (100001)
+print(bfs(n))
