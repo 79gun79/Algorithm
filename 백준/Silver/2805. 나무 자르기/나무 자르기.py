a9a255818a2ccc12cfe65afc9edc10fca716353e
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-01-10 11:19:18 +0900

    [Silver II] Title: 나무 자르기, Time: 548 ms, Memory: 258360 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,25 @@
 import sys
 input = sys.stdin.readline
 
+def binary_search(lst):
+    l = 1
+    r = sum(lst)
+    res = 0
+    while l <= r:
+        mid = (l + r) // 2
+        h = 0
+        for i in lst:
+            if i > mid:
+                h += (i - mid)
+
+        if h >= m:
+            res = mid
+            l = mid + 1
+        else:
+            r = mid - 1
+
+    return res
+
 n, m = map(int, input().split())
-h = list(map(int, input().split()))
-s = 1
-e = sum(h)
-
-while s <= e:
-    mid = (s+e) // 2
-    pnt = 0
-
-    for i in h:
-        if i > mid:
-            pnt += (i - mid)
-
-    if pnt >= m:
-        s = mid + 1
-    else:
-        e = mid - 1
-
-print(e)
+tr = list(map(int, input().split()))
+print(binary_search(tr))
