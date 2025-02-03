7a8495e79ff72d7dcaa4cca4b2e7c52c15f8e094
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-02-03 11:58:22 +0900

    [Silver III] Title: 이건 꼭 풀어야 해!, Time: 344 ms, Memory: 170768 KB -BaekjoonHub

--- 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
+++ 백준/Silver/17390. 이건 꼭 풀어야 해！/이건 꼭 풀어야 해！.py
@@ -3,11 +3,11 @@ input = sys.stdin.readline
 
 n, q = map(int, input().split())
 a = list(map(int, input().split()))
-b = sorted(a)
+num = [0] * 300001
 
-num = [0] * (300001)
+a.sort()
 for i in range(1, n+1):
-    num[i] = num[i-1] + b[i-1]
-for i in range(q):
+    num[i] = num[i-1] + a[i-1]
+for _ in range(q):
     l, r = map(int, input().split())
     print(num[r] - num[l-1])
