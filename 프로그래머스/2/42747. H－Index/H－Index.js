a9a2f38577e5069f8609384230db5fd28f86137e
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-04-09 20:37:11 +0900

    [level 2] Title: H-Index, Time: 0.65 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,9 +1,8 @@
 function solution(citations) {
-    const cit = citations.sort((a, b) => b - a)
-    for (let i = 0; i < cit.length; i++) {
-        if (cit[i] < i+1) {
-            return i
-        }
+    const citSort = citations.sort((a,b) => b - a);
+    
+    for (let i=0; i<citSort.length; i++) {
+        if (citSort[i] < i+1) return i;
     }
-    return cit.length
+    return citSort.length;
 }
