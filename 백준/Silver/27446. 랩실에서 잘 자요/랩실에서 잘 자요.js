a20112e2d9c9194dc7f6a4c0f0ffe12f6bbdf90e
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-09-02 14:43:34 +0900

    [Silver III] Title: 랩실에서 잘 자요, Time: 96 ms, Memory: 9352 KB -BaekjoonHub

--- 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.js
+++ 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.js
@@ -5,15 +5,14 @@ const input = require("fs")
   .split("\n")
   .map((v) => v.split(" ").map(Number));
 
-const [num, p] = input;
-const [n, m] = num;
-const pg = [...new Array(n)].map((_, i) => i + 1);
-const need = pg.filter((x) => !p.includes(x));
+const [[n, m], pg] = input;
+
+const page = [...new Array(n)].map((_, i) => i + 1);
+const prt = page.filter((v) => !pg.includes(v));
 
 let res = 0;
-for (let i = 0; i < need.length; i++) {
+for (let i = 0; i < prt.length; i++) {
   if (i === 0) res += 7;
-  else res += Math.min(7, 2 * (need[i] - need[i - 1]));
+  else res += Math.min(7, 2 * (prt[i] - prt[i - 1]));
 }
-
 console.log(res);
