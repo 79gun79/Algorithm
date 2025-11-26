9a2532f68e80d323c1a6dbda00516270505bd12d
j_gun2 <127276008+79gun79@users.noreply.github.com>
2025-11-26 16:55:20 +0900

    [Bronze III] Title: 학점계산, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.js
@@ -0,0 +1,24 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./example.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [score] = input;
+
+const dic = {
+  "A+": "4.3",
+  A0: "4.0",
+  "A-": "3.7",
+  "B+": "3.3",
+  B0: "3.0",
+  "B-": "2.7",
+  "C+": "2.3",
+  C0: "2.0",
+  "C-": "1.7",
+  "D+": "1.3",
+  D0: "1.0",
+  "D-": "0.7",
+  F: "0.0",
+};
+console.log(dic[score]);
